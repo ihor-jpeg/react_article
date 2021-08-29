30d44288dffb4784a222f6b28c43e500cef7ab98
Ihor Nahnybida <igor653279@gmail.com>
2021-08-29 16:12:11 +0300

    Updates

---
+++ precache-manifest.28b4f6d493c612049b74a6df3c98865c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba065447fe0decca5e6eb77ae90c12b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61cd6754cffa3b7f506b",
+    "url": "./static/js/main.a95b7bc6.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.28b4f6d493c612049b74a6df3c98865c.js"
 );
 
 self.addEventListener('message', (event) => {
